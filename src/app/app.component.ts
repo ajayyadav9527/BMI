d0529803138748d83d7046c005ad1a5ca7ae49f8
Ajay <yadavaj9527@gmail.com>
2024-07-17 00:04:12 +0530

    Body Mass Index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bmi-app';
+
+
+  height: number = 0;
+  weight: number = 0;
+  bmiResult: string = '';
+  bmiCategory: string = '';
+
+  calculateBMI() {
+    if (this.height <= 0 || this.weight <= 0) {
+      alert('Please enter valid numbers for height and weight.');
+      return;
+    }
+
+    const heightInMeters = this.height / 100;
+    const weightInKg = this.weight;
+
+    // Calculate BMI
+    const bmi = weightInKg / (heightInMeters * heightInMeters);
+
+    // Determine BMI category and result message
+    if (bmi < 18.5) {
+      this.bmiCategory = 'underweight';
+      this.bmiResult = `Your BMI is ${bmi.toFixed(2)}. You are underweight.`;
+    } else if (bmi < 24.9) {
+      this.bmiCategory = 'normal weight';
+      this.bmiResult = `Your BMI is ${bmi.toFixed(
+        2
+      )}. You have a normal weight.`;
+    } else if (bmi < 29.9) {
+      this.bmiCategory = 'overweight';
+      this.bmiResult = `Your BMI is ${bmi.toFixed(2)}. You are overweight.`;
+    } else {
+      this.bmiCategory = 'obese';
+      this.bmiResult = `Your BMI is ${bmi.toFixed(2)}. You are obese.`;
+    }
+  }
 }
